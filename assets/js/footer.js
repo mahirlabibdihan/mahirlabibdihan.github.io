8d04d260b005630f2abb6c38c34326cafe138277
mahirlabibdihan <mahirlabibdihan@gmail.com>
2021-08-02 17:32:24 +0600

    mahirlabibdihan

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,35 +1,13 @@
-function createSocialIcon(className,url){
-    let socialIcon = document.createElement('li');
-    socialIcon.classList.add("social-icon");
-    let socialIconLink = document.createElement('a');
-    socialIconLink.href=url;
-    socialIconLink.classList.add("fa");
-    socialIconLink.classList.add(className);
-    socialIcon.appendChild(socialIconLink);
-    return socialIcon;
-}
-
-function createSocialIconsList(){
-    let socialIconsList = document.createElement('ul');
-    socialIconsList.classList.add("social-icons-list");
-
-    socialIconsList.appendChild(createSocialIcon("fa-twitter","https://twitter.com/mahirlabibdihan"));
-    socialIconsList.appendChild(createSocialIcon("fa-facebook","https://facebook.com/mahirlabibdihan"));
-    socialIconsList.appendChild(createSocialIcon("fa-linkedin","https://linkedin.com/mahirlabibdihan"));
-    socialIconsList.appendChild(createSocialIcon("fa-instagram","https://instagram.com/di_huu"));
-    socialIconsList.appendChild(createSocialIcon("fa-github","https://github.com/mahirlabibdihan"));
-    socialIconsList.appendChild(createSocialIcon("fa-envelope","mailto:mahirlabibdihan@gmail.com"));
-
-    return socialIconsList;
-}
-
-function createCopyright(){
-    let copyright = document.createElement('h4');
-    copyright.classList.add("copyright");
-    copyright.appendChild(document.createTextNode('© Mahir Labib Dihan All rights reserved'));
-    return copyright;
-}
-
 const footer = document.querySelector(".footer");
-footer.appendChild(createSocialIconsList());
-footer.appendChild(createCopyright());
+footer.innerHTML+="" +
+    "<ul class=\"social-icons-list\">\n" +
+    "        <li class=\"social-icon\"><a href=\"https://twitter.com/mahirlabibdihan\" class=\"fa fa-twitter\"></a></li>\n" +
+    "        <li class=\"social-icon\"><a href=\"https://www.facebook.com/mahirlabibdihan/\" class=\"fa fa-facebook\"></a></li>\n" +
+    "        <li class=\"social-icon\"><a href=\"https://www.linkedin.com/in/mahirlabibdihan/\" class=\"fa fa-linkedin\"></a></li>\n" +
+    "        <li class=\"social-icon\"><a href=\"https://www.instagram.com/di_huu/\" class=\"fa fa-instagram\"></a></li>\n" +
+    "        <li class=\"social-icon\"><a href=\"https://github.com/mahirlabibdihan\" class=\"fa fa-github\"></a></li>\n" +
+    "        <li class=\"social-icon\"><a href=\"mailto:mahirlabibdihan@gmail.com\" class=\"fa fa-envelope\"></a></li>\n" +
+    "    </ul>\n" +
+    "    <h4 class=\"copyright\">\n" +
+    "        &copy; Mahir Labib Dihan All rights reserved\n" +
+    "    </h1>";
--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,52 +1,28 @@
-function createLine(){
-    let line=document.createElement('div');
-    line.classList.add("line");
-    return line;
-}
-function createNavButton(){
-    let navButton = document.createElement('div');
-    navButton.classList.add("nav-button");
-    navButton.appendChild(createLine());
-    navButton.appendChild(createLine());
-    navButton.appendChild(createLine());
-    return navButton;
-}
-
-function createNavBarLink(name,url){
-    let navBarLink = document.createElement('li');
-    navBarLink.classList.add("nav-bar-link");
-    let link = document.createElement('a');
-    link.href=url;
-    link.appendChild(document.createTextNode(name));
-    navBarLink.appendChild(link);
-    return navBarLink;
-}
-function createNavBarLeft(){
-    let navBarLeft = document.createElement('ul');
-    navBarLeft.classList.add("nav-bar-left");
-    navBarLeft.appendChild(createNavBarLink("HOME","/"));
-    navBarLeft.appendChild(createNavBarLink("WORK","/work"));
-    navBarLeft.appendChild(createNavBarLink("BLOG","/blog"));
-    navBarLeft.appendChild(createNavBarLink("ABOUT","/about"));
-    navBarLeft.appendChild(createNavBarLink("CONTACT","/contact"));
-    return navBarLeft;
-}
-
-function createNavBarRight(){
-    let navBarRight = document.createElement('div');
-    navBarRight.classList.add("nav-bar-right");
-    // let navBarName = document.createElement('div');
-    // navBarName.classList.add("nav-bar-name");
-    // navBarName.appendChild(document.createTextNode("MAHIR LABIB DIHAN"));
-    // navBarRight.appendChild(navBarName);
-    return navBarRight;
-}
-function createNavBar(){
-    const navBar = document.querySelector(".nav-bar");
-    navBar.appendChild(createNavButton());
-    navBar.appendChild(createNavBarLeft());
-    navBar.appendChild(createNavBarRight());
-}
-
-createNavBar();
-
+const navBar = document.querySelector(".nav-bar");
+navBar.innerHTML+="" +
+    "<div class=\"nav-button\">\n" +
+    "                    <div class=\"line\"></div>\n" +
+    "                    <div class=\"line\"></div>\n" +
+    "                    <div class=\"line\"></div>\n" +
+    "                </div>\n" +
+    "                <ul  class=\"nav-bar-left\">\n" +
+    "                    <li class=\"nav-bar-link\">\n" +
+    "                        <a href=\"/\">HOME</a>\n" +
+    "                    </li>\n" +
+    "                    \n" +
+    "                    <li class=\"nav-bar-link\">\n" +
+    "                        <a href=\"/work\">WORK</a>\n" +
+    "                    </li>\n" +
+    "                    <li class=\"nav-bar-link\">\n" +
+    "                        <a href=\"/blog\">BLOG</a>\n" +
+    "                    </li>\n" +
+    "                    <li class=\"nav-bar-link\">\n" +
+    "                        <a href=\"/about\">ABOUT</a>\n" +
+    "                    </li>\n" +
+    "                    <li class=\"nav-bar-link\">\n" +
+    "                        <a href=\"/contact\">CONTACT</a>\n" +
+    "                    </li>\n" +
+    "                </ul>\n" +
+    "            <div class=\"nav-bar-right\">\n" +
+    "                \n" +
+    "            </div>";
