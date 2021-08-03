3e55e8a767b6644b97f473db8fab591fa4b50d85
mahirlabibdihan <mahirlabibdihan@gmail.com>
2021-08-04 01:16:43 +0600

    mahirlabibdihan

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,13 +1,13 @@
 const footer = document.querySelector(".footer");
-footer.innerHTML+="" +
-    "<ul class=\"social-icons-list\">\n" +
-    "        <li class=\"social-icon\"><a href=\"https://twitter.com/mahirlabibdihan\" class=\"fa fa-twitter\"></a></li>\n" +
-    "        <li class=\"social-icon\"><a href=\"https://www.facebook.com/mahirlabibdihan/\" class=\"fa fa-facebook\"></a></li>\n" +
-    "        <li class=\"social-icon\"><a href=\"https://www.linkedin.com/in/mahirlabibdihan/\" class=\"fa fa-linkedin\"></a></li>\n" +
-    "        <li class=\"social-icon\"><a href=\"https://www.instagram.com/di_huu/\" class=\"fa fa-instagram\"></a></li>\n" +
-    "        <li class=\"social-icon\"><a href=\"https://github.com/mahirlabibdihan\" class=\"fa fa-github\"></a></li>\n" +
-    "        <li class=\"social-icon\"><a href=\"mailto:mahirlabibdihan@gmail.com\" class=\"fa fa-envelope\"></a></li>\n" +
-    "    </ul>\n" +
-    "    <h4 class=\"copyright\">\n" +
-    "        &copy; Mahir Labib Dihan All rights reserved\n" +
-    "    </h1>";
+footer.innerHTML+='' +
+    '<ul class="social-icons-list">\n' +
+    '        <li class="social-icon"><a href="https://twitter.com/mahirlabibdihan" class="fa fa-twitter"></a></li>\n' +
+    '        <li class="social-icon"><a href="https://www.facebook.com/mahirlabibdihan/" class="fa fa-facebook"></a></li>\n' +
+    '        <li class="social-icon"><a href="https://www.linkedin.com/in/mahirlabibdihan/" class="fa fa-linkedin"></a></li>\n' +
+    '        <li class="social-icon"><a href="https://www.instagram.com/di_huu/" class="fa fa-instagram"></a></li>\n' +
+    '        <li class="social-icon"><a href="https://github.com/mahirlabibdihan" class="fa fa-github"></a></li>\n' +
+    '        <li class="social-icon"><a href="mailto:mahirlabibdihan@gmail.com" class="fa fa-envelope"></a></li>\n' +
+    '    </ul>\n' +
+    '    <h4 class="copyright">\n' +
+    '        &copy; Mahir Labib Dihan All rights reserved\n' +
+    '    </h4>';
--- assets/js/nav.js
+++ assets/js/nav.js
@@ -17,32 +17,21 @@ window.onload = function()
     document.querySelector(".container").style.opacity = "100";
 }
 
-
-document.querySelector(".nav-bar").innerHTML+="" +
-    "<div class=\"nav-button\">\n" +
-    "                    <div class=\"line\"></div>\n" +
-    "                    <div class=\"line\"></div>\n" +
-    "                    <div class=\"line\"></div>\n" +
-    "                </div>\n" +
-    "                <ul  class=\"nav-bar-left\">\n" +
-    "                    <li id=\"home-tab\" class=\"nav-bar-link\">\n" +
-    "                        <a href=\"/\">HOME</a>\n" +
-    "                    </li>\n" +
-    "                    <li id=\"work-tab\" class=\"nav-bar-link\">\n" +
-    "                        <a href=\"/work\">WORK</a>\n" +
-    "                    </li>\n" +
-    "                    <li id=\"blog-tab\" class=\"nav-bar-link\">\n" +
-    "                        <a href=\"/blog\">BLOG</a>\n" +
-    "                    </li>\n" +
-    "                    <li id=\"about-tab\" class=\"nav-bar-link\">\n" +
-    "                        <a href=\"/about\">ABOUT</a>\n" +
-    "                    </li>\n" +
-    "                    <li id=\"contact-tab\" class=\"nav-bar-link\">\n" +
-    "                        <a href=\"/contact\">CONTACT</a>\n" +
-    "                    </li>\n" +
-    "                </ul>\n" +
-    "                   <div class=\"nav-bar-right\">\n" +
-    "                <div class=\"nav-bar-name\"><a href=\"https://mahirlabibdihan.github.io\"></a></div>\n" +
-    "            </div>" ;
+document.querySelector(".nav-bar").innerHTML+= '\
+    <div class="nav-button">\
+        <div class="line"></div>\
+        <div class="line"></div>\
+        <div class="line"></div>\
+    </div>\
+    <ul class="nav-bar-left">\
+        <li id="home-tab" class="nav-bar-link"><a href="/">HOME</a> </li>\
+        <li id="work-tab" class="nav-bar-link"> <a href="/work">WORK</a> </li>\
+        <li id="blog-tab" class="nav-bar-link"> <a href="/blog">BLOG</a></li>\
+        <li id="about-tab" class="nav-bar-link"> <a href="/about">ABOUT</a></li>\
+        <li id="contact-tab" class="nav-bar-link"> <a href="/contact">CONTACT</a></li>\
+    </ul>\
+    <div class="nav-bar-right">\
+    <div class="nav-bar-name"><a href="https://mahirlabibdihan.github.io"></a></div>\
+    </div>';
 
 document.querySelector(".nav-button").addEventListener("click",toggle);
