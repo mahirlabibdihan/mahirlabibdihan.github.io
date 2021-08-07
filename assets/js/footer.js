3c9c8c2047e6f8b31b77f00ed4e6b8ae8dbc3516
mahirlabibdihan <mahirlabibdihan@gmail.com>
2021-08-07 15:18:06 +0600

    mahirlabibdihan

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,13 +1,14 @@
 const footer = document.querySelector(".footer");
 footer.innerHTML += '' +
     '<ul class="social-icons-list">\n' +
-    '        <li class="social-icon"><a href="https://twitter.com/mahirlabibdihan" class="fa fa-twitter"></a></li>\n' +
-    '        <li class="social-icon"><a href="https://www.facebook.com/mahirlabibdihan/" class="fa fa-facebook"></a></li>\n' +
-    '        <li class="social-icon"><a href="https://www.linkedin.com/in/mahirlabibdihan/" class="fa fa-linkedin"></a></li>\n' +
-    '        <li class="social-icon"><a href="https://www.instagram.com/di_huu/" class="fa fa-instagram"></a></li>\n' +
-    '        <li class="social-icon"><a href="https://github.com/mahirlabibdihan" class="fa fa-github"></a></li>\n' +
-    '        <li class="social-icon"><a href="mailto:mahirlabibdihan@gmail.com" class="fa fa-envelope"></a></li>\n' +
-    '    </ul>\n' +
-    '    <h4 class="copyright">\n' +
-    '        &copy; Mahir Labib Dihan All rights reserved\n' +
-    '    </h4>';
+    '        <li class="social-icon"><a href="https://twitter.com/mahirlabibdihan" class="icons fa fa-twitter"></a></li>\n' +
+    '        <li class="social-icon"><a href="https://www.facebook.com/mahirlabibdihan/" class="icons fa fa-facebook"></a></li>\n' +
+    '        <li class="social-icon"><a href="https://www.linkedin.com/in/mahirlabibdihan/" class="icons fa fa-linkedin"></a></li>\n' +
+    '        <li class="social-icon"><a href="https://www.instagram.com/di_huu/" class="icons fa fa-instagram"></a></li>\n' +
+    '        <li class="social-icon"><a href="https://github.com/mahirlabibdihan" class="icons fa fa-github"></a></li>\n' +
+    '        <li class="social-icon"><a href="https://www.youtube.com/channel/UCTqeUw6A6cKjpNXaAcveGYQ" class="icons fa fa-youtube"></a></li>\n' +
+    '        <li class="social-icon"><a href="mailto:mahirlabibdihan@gmail.com" class="icons fa fa-envelope"></a></li>\n' +
+    '</ul>\n' +
+    '<h4 class="copyright">\n' +
+    '    &copy; Mahir Labib Dihan All rights reserved\n' +
+    '</h4>';
