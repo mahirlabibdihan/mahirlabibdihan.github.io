ae627f68ff7b93698606dfc11209297997d88c95
mahirlabibdihan <mahirlabibdihan@gmail.com>
2021-08-09 01:54:15 +0600

    mahirlabibdihan

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,12 +1,12 @@
 const footer = document.querySelector(".footer");
 footer.innerHTML += '' +
     '<ul class="social-icons-list">\n' +
-    '        <li class="social-icon"><a href="https://twitter.com/mahirlabibdihan" class="icons fa fa-twitter"></a></li>\n' +
-    '        <li class="social-icon"><a href="https://www.facebook.com/mahirlabibdihan/" class="icons fa fa-facebook"></a></li>\n' +
-    '        <li class="social-icon"><a href="https://www.linkedin.com/in/mahirlabibdihan/" class="icons fa fa-linkedin"></a></li>\n' +
-    '        <li class="social-icon"><a href="https://www.instagram.com/di_huu/" class="icons fa fa-instagram"></a></li>\n' +
-    '        <li class="social-icon"><a href="https://github.com/mahirlabibdihan" class="icons fa fa-github"></a></li>\n' +
-    '        <li class="social-icon"><a href="https://www.youtube.com/channel/UCTqeUw6A6cKjpNXaAcveGYQ" class="icons fa fa-youtube"></a></li>\n' +
+    '        <li class="social-icon"><a href="https://twitter.com/mahirlabibdihan" class="icons fab fa-twitter"></a></li>\n' +
+    '        <li class="social-icon"><a href="https://www.facebook.com/mahirlabibdihan/" class="icons fab fa-facebook-f"></a></li>\n' +
+    '        <li class="social-icon"><a href="https://www.linkedin.com/in/mahirlabibdihan/" class="icons fab fa-linkedin"></a></li>\n' +
+    '        <li class="social-icon"><a href="https://www.instagram.com/di_huu/" class="icons fab fa-instagram"></a></li>\n' +
+    '        <li class="social-icon"><a href="https://github.com/mahirlabibdihan" class="icons fab fa-github"></a></li>\n' +
+    '        <li class="social-icon"><a href="https://www.youtube.com/channel/UCTqeUw6A6cKjpNXaAcveGYQ" class="icons fab fa-youtube"></a></li>\n' +
     '        <li class="social-icon"><a href="mailto:mahirlabibdihan@gmail.com" class="icons fa fa-envelope"></a></li>\n' +
     '</ul>\n' +
     '<h4 class="copyright">\n' +
